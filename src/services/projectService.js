7867bdffd935a71a2aa12e31c0f413503a707192
r51019765-prog <r51019765@gmail.com>
2025-09-02 22:02:31 +0530

    second commit

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
+
 const API = import.meta.VITE_API_URL || 'https://portifolio-demo.onrender.com/api/projects';
-=======
-const API = 'http://localhost:8080/api/projects';
->>>>>>> f2b41d72482c1015fe6a2d85c8943620633578ed
+
+
+
 
 export const getAll = () => axios.get(API);
 
